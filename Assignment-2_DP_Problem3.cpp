c358578f814990f9c5a28443eecf01a32f7645d1
shwetamaurya13 <shwetamaurya1995@gmail.com>
2017-02-27 23:21:06 +0530

    Update Assignment-2_DP_Problem3.cpp

--- Assignment-2_DP_Problem3.cpp
+++ Assignment-2_DP_Problem3.cpp
@@ -65,6 +65,18 @@ int Function(int a[], int n)
 }
 
 int main(){
+    /*------FOR MULTIPLE TEST CASES----
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int j=0; j<n; j++)
+            cin>>a[j];
+        cout<<Function(a,n)<<endl;
+    } */
     int a[]={4,8,6,1,5,2};
    cout<<Function(a,6);
     return 0;
